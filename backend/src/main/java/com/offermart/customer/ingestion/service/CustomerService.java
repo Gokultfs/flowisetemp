53f78ba575b01f81c43c9a9a771daf44a197bec2
Gokul <gokuls1@ltfs.com>
2025-06-18 10:47:52 +0530

    feat: Add CustomerService interface

---
+++ backend/src/main/java/com/offermart/customer/ingestion/service/CustomerService.java
@@ -0,0 +1,8 @@
+package com.offermart.customer.ingestion.service;
+import com.offermart.customer.ingestion.model.Customer;
+import java.util.List;
+
+
+public interface CustomerService {
+    List<Customer> processCustomerData(List<Customer> raw éstomers);
+}
