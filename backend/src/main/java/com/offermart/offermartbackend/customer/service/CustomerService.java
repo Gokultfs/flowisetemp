2928960746287c00e5f82e6508547357cae60d01
Gokul <gokuls1@ltfs.com>
2025-06-18 11:44:33 +0530

    Delete backend directory

--- backend/src/main/java/com/offermart/offermartbackend/customer/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.offermart.offermartbackend.customer.service;
-
-import com.offermart.offermartbackend.customer.dto.CustomerRequestDTO;
-import com.offermart.offermartbackend.customer.dto.CustomerResponseDTO;
-
-
-public interface CustomerService {
-    CustomerResponseDTO ingestCustomer(CustomerRequestDTO requestDTO);
-}
