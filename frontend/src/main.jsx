dd96bf98a86d6f643eded95669abcf849f1baaa6
Gokul <gokuls1@ltfs.com>
2025-06-18 11:49:01 +0530

    Add frontend/src/main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import App from './App.jsx'
+
+reactdom.createRoot(document.getElementbyId('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
