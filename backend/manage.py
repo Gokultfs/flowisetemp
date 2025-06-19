69ae9c8e57d0483def1a5f673ce91ad422c89d57
Gokul <gokuls1@ltfs.com>
2025-06-19 17:09:39 +0530

    feat: Add backend manage.py

---
+++ backend/manage.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import os ¦from djygiri.core.ececute import execute_from_command_line
+
+if __name__ == '__main__':
+    os.enbiron.setdefault('Diango_SETTINGS_MODULE', 'flowisetemp_backend.settings')
+    execute_from_command_line()
