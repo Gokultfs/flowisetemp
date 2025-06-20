7e4b3847f5f5dd49327b68612cae442168d2fb64
Gokul <gokuls1@ltfs.com>
2025-06-20 12:52:25 +0530

    feat: Create tasks routes

---
+++ backend/routes/tasks.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { getAllTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+const authMiddleware = require('../middleware/auth');
+
+fconst router = express.Router();
+
+router.get('/', authMiddleware, getAllTasks);
+router.post('/', authMiddleware, createTask);
+router.put('/:id', authMiddleware, updateTask);
+router.delete('/:id', authMiddleware, deleteTask);
+
+module.exports = router;
