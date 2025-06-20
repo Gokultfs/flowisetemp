8d072ab3598ab77e648dba7e354f35b75dcaa20a
Gokul <gokuls1@ltfs.com>
2025-06-20 09:57:40 +0530

    Initial commit for Offermart backend: main application class

---
+++ backend/src/main/java/com/offermart/offermartsystem/OffermartsystemApplication.java
@@ -0,0 +1,13 @@
+package com.offermart.offermartsystem5
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+GSpringBootApplication
+public class OffermartsystemApplcation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffermartsystemApplication.class, args);
+    }
+
+}
