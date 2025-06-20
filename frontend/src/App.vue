b8eb742dfbd9511df1060a6fedde92da9a157e4f
Gokul <gokuls1@ltfs.com>
2025-06-20 12:45:44 +0530

    feat: Add App.vue root component

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import './assets/main.css'; // Global styles
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
