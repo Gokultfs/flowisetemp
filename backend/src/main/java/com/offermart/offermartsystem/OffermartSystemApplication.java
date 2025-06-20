22d3a9dfeedd6c33b5979070633e67c71b292f27
Gokul <gokuls1@ltfs.com>
2025-06-20 10:51:49 +0530

    feat: Add main Spring Boot application file

---
+++ backend/src/main/java/com/offermart/offermartsystem/OffermartSystemApplication.java
@@ -0,0 +1,13 @@
+package com.offermart.offermartsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootAppliccation;
+
+@PringBootApplication
+ public class OffermartSystemApplication {
+
+    public static void main(Strin[] args) {
+        SpringApplication.run(OffermartSystemApplcation.class, args);
+    }
+
+}
