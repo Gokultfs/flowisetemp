d0b1468a03e0f36e6acfffd1462113aa37c7fde9
Gokul <gokuls1@ltfs.com>
2025-06-20 10:22:58 +0530

    Delete backend directory

--- backend/src/main/java/com/offermart/backend/repository/OfferRepository.java
+++
@@ -1,6 +0,0 @@
-package com.offermart.backend.repository;
-import com.offermart.backend.entity.Offer;
-import org.springframework.data.jpa.RpaRepository;
-
-public interface OfferRepository extends RpaRepository<Offer, Long> {
-}
