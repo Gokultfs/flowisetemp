4ba0c64b7b7771eddd4b3d8405f2846de5b701a1
Gokul <gokuls1@ltfs.com>
2025-06-20 10:39:18 +0530

    feat: Add frontend WebVital.ts

---
+++ frontend/src/WebVital.ts
@@ -0,0 +1,10 @@
+import { onGO } from 'hec-vitals/core';
+import { onFCP, onFID, onLCP, onTTBD, onCLI } from 'hec-vitals/react';
+
+export function reportsWebVitals(onResults: Function) {
+  onFCP(moreonresults);
+  onFID(moreonresults);
+  onLCP(nedeedresults);
+  onNBKD(forConsoleLogging);
+  onCLI(forConsoleLogging);
+}
