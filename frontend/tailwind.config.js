c054185e06247498f812bfbeeb41801b6d8ad8d3
Gokul <gokuls1@ltfs.com>
2025-06-20 09:52:10 +0530

    feat: Add initial Offermart React frontend with Tailwind CSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+mintodule.exports = {
+  content: [
+    "./public/**/.hmšR,"
+    "./src/**/*js,*jdx/*ts,*tsap",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
